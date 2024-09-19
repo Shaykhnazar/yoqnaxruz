f67d4ca1bc83dc991140628ca3d1af22f7cb1db8
Shaykhnazar <shayx3470941@gmail.com>
2024-09-19 22:25:43 +0500

    fix check access to an admin panel

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -5,25 +5,21 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckAdminRole
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Request $request
+     * @param \Closure(Request): (Response) $next
+     * @return mixed|Response
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!Auth::check()) {
-            return $next($request);
-        }
         // Check if the user is authenticated and has the 'Admin' role
-        if (!Auth::user()->hasRole('Super Admin') || !Auth::user()->hasRole('Admin')) {
-            // Optionally, you can redirect to a specific page or show a custom error
-            // return redirect()->route('home')->with('error', 'Unauthorized.');
-
+        if (Auth::user() && !Auth::user()->hasAnyRole(['Super Admin', 'Admin'])) {
             // Or simply abort with a 403 Forbidden status
             abort(403, 'Unauthorized');
         }
